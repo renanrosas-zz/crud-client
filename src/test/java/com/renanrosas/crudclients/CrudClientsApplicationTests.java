cb3e5320b0f2e86c4a616b77a3970d9bfb888fa1
Renan Rosas <renan-rosas@hotmail.com>
2021-05-15 21:33:24 -0300

    Project created, Client

---
+++ src/test/java/com/renanrosas/crudclients/CrudClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renanrosas.crudclients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
